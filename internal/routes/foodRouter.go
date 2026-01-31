92c8ac5f11e0ae8ca662cc596fd034c3360994cc
Arab45 <horlakunleolayiwola@gmail.com>
2026-01-31 00:13:05 -0800

    refactoring

--- internal/routes/foodRouter.go
+++ internal/routes/foodRouter.go
@@ -1,22 +1,26 @@
 package routes
 
-import(
+import (
+	controller "RESTAURANT-MANAGEMENT/internal/controller"
+	_ "RESTAURANT-MANAGEMENT/internal/model"
 	"github.com/gin-gonic/gin"
-	controller"RESTAURANT-MANAGEMENT/internal/controller"
+	_ "go.mongodb.org/mongo-driver/mongo"
 )
-func FoodRouter(incomingRouter *gin.RouterGroup){
+
+func FoodRouter(incomingRouter *gin.RouterGroup) {
 	// CreateFood godoc
-// @Summary      Create a new food item
-// @Description  Create a food item and attach it to a menu
-// @Tags         Foods
-// @Accept       json
-// @Produce      json
-// @Param        food body model.FoodModel true "Food payload"
-// @Success      200 {object} mongo.InsertOneResult
-// @Failure      400 {object} map[string]string
-// @Failure      500 {object} map[string]string
-// @Router       /api/v1/food [post]
+	// @Summary      Create a new food item
+	// @Description  Create a food item and attach it to a menu
+	// @Tags         Foods
+	// @Accept       json
+	// @Produce      json
+	// @Param        food body model.FoodModel true "Food payload"
+	// @Success      200 {object} mongo.InsertOneResult
+	// @Failure      400 {object} map[string]string
+	// @Failure      500 {object} map[string]string
+	// @Router       /api/v1/food [post]
 	incomingRouter.POST("/food", controller.CreateFood())
+
 	incomingRouter.GET("/foods", controller.GetFoods())
 	incomingRouter.GET("/food/:id", controller.GetFood())
 	incomingRouter.PUT("/food/:id", controller.UpdateFood())
