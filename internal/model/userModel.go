f0db111649d11d45a1374dad41ca099e7fdbe1cd
Arab45 <horlakunleolayiwola@gmail.com>
2026-01-31 04:46:50 -0800

    refactoring

--- internal/model/userModel.go
+++ internal/model/userModel.go
@@ -8,12 +8,12 @@ import (
 
 type UserModel struct {
 	ID            primitive.ObjectID `bson:"_id"`
-	First_name    *string            `json:"first_name" validate:"required,min=2,max-100"`
+	First_name    *string            `json:"first_name" validate:"required,min=2,max=100"`
 	Last_name     *string            `json:"last_name" validate:"required,min=2,max=100"`
 	Password      *string            `json:"password" validate:"required,min=6"`
 	Email         *string            `json:"email" validate:"required"`
 	Avatar        *string            `json:"avatar"`
-	Phone         *string            `json:"phone" validate:"required"`
+	Phone         *string            `json:"phone" validate:"required,min=10,max=15"`
 	Token         *string            `json:"token"`
 	Refresh_Token *string            `json:"refresh_token"`
 	Created_at    time.Time          `json:"created_at"`
